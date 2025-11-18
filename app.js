b4473dbd41c277dced68c189e180e85a61afbff0
itznotsx <shangxian.lee@gmail.com>
2025-11-18 09:01:15 +0800

    change name

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
-const timetableController = require("./ShangXian_classes.js");
+const classController = require("./ShangXian_classes.js");
 
 // Add Class
 console.log("");
-const addedClass = timetableController.addClass("Workplace Digital Skills", "EGC155", "10/11/2025","2:00","Jim");
+const addedClass = classController.addClass("Workplace Digital Skills", "EGC155", "10/11/2025","2:00","Jim");
 console.log(addedClass + " added");
 console.log("");
 
 // Reschedule classes
-timetableController.rescheduleClass("Calculus", "12/11/2025", "09:30");
+classController.rescheduleClass("Calculus", "12/11/2025", "09:30");
 // List classes to show rescheduled class
 console.log("Updated timetable:");
-timetableController.listClass().forEach((cls, index) => {
+classController.listClass().forEach((cls, index) => {
   console.log(`${index + 1}. ${cls.modName} [${cls.modCode}] - Date: ${cls.date} | Time: ${cls.time} / Tutor: ${cls.tutor}`);
 });
 console.log("");
 
 // Find class by modName
-const mod = timetableController.searchClass("electronics");
+const mod = classController.searchClass("electronics");
 console.log("Search Results:")
 mod.forEach((cls, index) => {
   console.log(`${index + 1}. ${cls.modName} [${cls.modCode}] - Date: ${cls.date} | Time: ${cls.time} / Tutor: ${cls.tutor}`)
@@ -24,13 +24,13 @@ mod.forEach((cls, index) => {
 console.log("");
 
 // Remove class by modCode
-const remove = timetableController.removeClass("Workplace Digital Skills");
+const remove = classController.removeClass("Workplace Digital Skills");
 console.log(`Removed class: ${remove.modName}`);
 console.log("");
 
 // List all classes
 console.log("Classes list:")
-timetableController.listClass().forEach((cls, index) => {
+classController.listClass().forEach((cls, index) => {
   console.log(`${index + 1}. ${cls.modName} [${cls.modCode}] - Date: ${cls.date} | Time: ${cls.time} / Tutor: ${cls.tutor}`);
 });
 console.log("");
